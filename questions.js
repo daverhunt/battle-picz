759768a74ed827744f69998cd8c1e2c385e8572f
daverhunt <daverhunt@gmail.com>
2026-09-07 22:27:12 +0100

    Add generated picture question library

---
+++ questions.js
@@ -0,0 +1,38 @@
+window.BATTLE_PICZ_QUESTIONS = {
+  ANIMALS: [
+    { a: "REDPANDA", label: "RED PANDA", pic: "🐾", image: "assets/images/animals/red-panda.webp", gt: 7200, gs: 3710 },
+    { a: "ELEPHANT", pic: "🐘", image: "assets/images/animals/elephant.webp", gt: 6600, gs: 3890 },
+    { a: "GIRAFFE", pic: "🦒", image: "assets/images/animals/giraffe.webp", gt: 6900, gs: 3820 },
+    { a: "TIGER", pic: "🐯", image: "assets/images/animals/tiger.webp", gt: 6100, gs: 4030 },
+    { a: "PENGUIN", pic: "🐧", image: "assets/images/animals/penguin.webp", gt: 6500, gs: 3910 },
+    { a: "OCTOPUS", pic: "🐙", image: "assets/images/animals/octopus.webp", gt: 7600, gs: 3590 },
+    { a: "FLAMINGO", pic: "🦩", image: "assets/images/animals/flamingo.webp", gt: 7900, gs: 3520 },
+    { a: "CHAMELEON", pic: "🦎", image: "assets/images/animals/chameleon.webp", gt: 8500, gs: 3380 },
+    { a: "HEDGEHOG", pic: "🦔", image: "assets/images/animals/hedgehog.webp", gt: 7800, gs: 3540 },
+    { a: "OWL", pic: "🦉", image: "assets/images/animals/owl.webp", gt: 5800, gs: 4140 }
+  ],
+  FOOD: [
+    { a: "PANCAKES", pic: "🥞", image: "assets/images/food/pancakes.webp", gt: 6900, gs: 3820 },
+    { a: "PIZZA", pic: "🍕", image: "assets/images/food/pizza.webp", gt: 5200, gs: 4340 },
+    { a: "BURGER", pic: "🍔", image: "assets/images/food/burger.webp", gt: 5600, gs: 4210 },
+    { a: "SUSHI", pic: "🍣", image: "assets/images/food/sushi.webp", gt: 6200, gs: 4000 },
+    { a: "STRAWBERRY", pic: "🍓", image: "assets/images/food/strawberry.webp", gt: 7400, gs: 3660 },
+    { a: "CROISSANT", pic: "🥐", image: "assets/images/food/croissant.webp", gt: 7900, gs: 3510 },
+    { a: "TACO", pic: "🌮", image: "assets/images/food/taco.webp", gt: 5900, gs: 4110 },
+    { a: "ICECREAM", label: "ICE CREAM", pic: "🍦", image: "assets/images/food/ice-cream.webp", gt: 7000, gs: 3780 },
+    { a: "SPAGHETTI", pic: "🍝", image: "assets/images/food/spaghetti.webp", gt: 7600, gs: 3600 },
+    { a: "AVOCADO", pic: "🥑", image: "assets/images/food/avocado.webp", gt: 6700, gs: 3870 }
+  ],
+  SPORT: [
+    { a: "FOOTBALL", pic: "⚽", image: "assets/images/sport/football.webp", gt: 5800, gs: 4140 },
+    { a: "TENNIS", pic: "🎾", image: "assets/images/sport/tennis.webp", gt: 5700, gs: 4180 },
+    { a: "BASKETBALL", pic: "🏀", image: "assets/images/sport/basketball.webp", gt: 7100, gs: 3750 },
+    { a: "BOXING", pic: "🥊", image: "assets/images/sport/boxing.webp", gt: 6500, gs: 3910 },
+    { a: "SKATEBOARD", pic: "🛹", image: "assets/images/sport/skateboard.webp", gt: 7600, gs: 3600 },
+    { a: "GOLF", pic: "⛳", image: "assets/images/sport/golf.webp", gt: 5400, gs: 4280 },
+    { a: "RUGBY", pic: "🏉", image: "assets/images/sport/rugby.webp", gt: 6100, gs: 4040 },
+    { a: "BADMINTON", pic: "🏸", image: "assets/images/sport/badminton.webp", gt: 8200, gs: 3440 },
+    { a: "CRICKET", pic: "🏏", image: "assets/images/sport/cricket.webp", gt: 6700, gs: 3870 },
+    { a: "BOWLING", pic: "🎳", image: "assets/images/sport/bowling.webp", gt: 6200, gs: 3990 }
+  ]
+};
